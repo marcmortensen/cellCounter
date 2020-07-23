b177847948bb55098c2ca16d83bf03f3f13ae148
Marc Mortensen <marcmortensen77@gmail.com>
2020-07-23 20:21:16 +0200

    Adding image loader

---
+++ __tests__/src/imageLoader/imageLoader.test.ts
@@ -0,0 +1,37 @@
+import { loadImagesFromPath } from "../../../src/imageLoader/imageLoader";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs'); 
+
+describe('Testing loading image path function', () => {
+  
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  it('loadImagesFromPath should search all the imgs inside a folder return their path', () => {
+
+    const mockReaddirSync = jest.spyOn(fs, 'readdirSync');
+    mockReaddirSync
+      .mockReturnValueOnce(['FolderName_1', 'image3.jpeg', 'image4.jpg'])
+      .mockReturnValueOnce(['FolderName_2', 'file.txt', 'image2.png'])
+      .mockReturnValue(['image1.png']);
+
+    const mockLastatSync = jest.spyOn(fs, 'lstatSync');
+
+    mockLastatSync.mockImplementation((path: string) => {
+      return { 
+        isDirectory: (): boolean => { return !path.includes('.');} 
+      };
+    });
+
+    expect(loadImagesFromPath('/fooPath')).toEqual([
+      '/fooPath/FolderName_1/FolderName_2/image1.png',
+      '/fooPath/FolderName_1/image2.png',
+      '/fooPath/image3.jpeg',
+      '/fooPath/image4.jpg'
+    ]);
+
+  });
+
+});
---
+++ src/imageLoader/imageLoader.ts
@@ -0,0 +1,20 @@
+import {readdirSync, lstatSync } from 'fs';
+import {join} from 'path';
+
+const loadImagesFromPath = (dir: string): Array<string>  => {
+    let images: Array<string> = [];
+    const files = readdirSync(dir)
+    files.forEach(function (file) {
+  
+      const fullPath = join(dir, file)
+      if (lstatSync(fullPath).isDirectory()){
+        images = images.concat(loadImagesFromPath(fullPath));
+      }
+      if ( /\.(jpe?g|png|gif|bmp)$/i.test(fullPath) ) {
+        images.push(fullPath)
+      }
+    })
+    return images;
+  }
+
+  export {loadImagesFromPath};
